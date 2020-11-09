d353775bbf06d044631f467ef3055450a81ef9ea
Davis Booth <davis.booth@duke.edu>
2020-11-09 18:53:27 -0500

    Finished enabling user play.

--- gym_test.py
+++ gym_test.py
@@ -1,11 +1,51 @@
+# Game.
 import gym
 import numpy as np
+
+# User.
+import keyboard
+import time
+
+# Init.
 env = gym.make('SpaceInvaders-v0')
 env.reset()
-print(np.array(env.reset()).flatten())
-print(env.action_space)
-print(env.observation_space)
+
+# Launch.
+env.render()
+i = 0
 while True:
+    # Render.
     env.render()
-    # print(env.action_space.sample())
-    env.step(env.action_space.sample())
+
+    # Delay.
+    if i == 0:
+        time.sleep(5)
+    else:
+        time.sleep(0.025)
+
+    # Listen.
+    try:
+        if keyboard.is_pressed('space'):  # Shoot.
+            env.step(1)
+            continue
+        elif keyboard.is_pressed('right'):  # Right.
+            env.step(2)
+            continue
+        elif keyboard.is_pressed('left'):  # Left.
+            env.step(3)
+            continue
+        elif keyboard.is_pressed('right') and keyboard.is_pressed('space'):  # Right + Shoot.
+            env.step(4)
+            continue
+        elif keyboard.is_pressed('left') and keyboard.is_pressed('space'):  # Left + Shoot.
+            env.step(5)
+            continue
+        
+        # Default.
+        env.step(0)
+        
+        # Increment.
+        i+=1
+    except:         # Sanity.
+        continue
+    
