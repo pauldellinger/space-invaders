0b9331f11e326142327d121f5bd5374d7e7db251
Davis Booth <davis.booth@duke.edu>
2020-11-10 23:28:17 -0500

    Last minute bugs.

--- geneticAlgo.py
+++ geneticAlgo.py
@@ -91,7 +91,7 @@ class geneticAlgorithm(object):
         for specs in paramList:
             self.SELECTIVITY = specs[1]                     # Set Selectivity.
             self.strategies = []                            # Clear.
-            for i in range(0, NUM_STRATEGIES):
+            for i in range(0, self.NUM_STRATEGIES):
                 s = Strategy(MUTATE_PROBABILITY=specs[0])   # Set Mutability.
                 self.strategies.append(s)
             
@@ -99,7 +99,7 @@ class geneticAlgorithm(object):
             results = self.startSimulation()
             
             # Stats.
-            self.export_results(results)
+            #self.export_results(results)
             stats = fetch_simulation_stats(results)
             
             # Append.
@@ -116,12 +116,11 @@ class geneticAlgorithm(object):
                 Ys.append([])
                 for j in range(len(allSimStats[i])):
                     Ys[i].append(allSimStats[i][j][stat])
-                Xs.append([range(len(allSimStats[i]))])
-
+                Xs.append(range(len(allSimStats[i])))
             d = {
                 
-                "X": Xs,
-                "Y":Ys,
+                "Xs": Xs,
+                "Ys":Ys,
                 "title" : variable + "'s effect on " + stat + "score",
                 "xLabel" : "Generation",
                 "yLabel" : stat + "Score",
@@ -129,7 +128,7 @@ class geneticAlgorithm(object):
                 "legendLabels" : legendLabels
             }
             plt = make_plot(d['title'], d['xLabel'], d['yLabel'])
-            compiled_results = zip(d['Xs'], d['Ys'], d['legendLabels'])
+            compiled_results = list(zip(d['Xs'], d['Ys'], d['legendLabels']))
                 #compiled_results = (d['Xs'][i], d['Ys'][i], label)
             add_lines(plt, compiled_results)
             save_plot(plt, stat + variable + self.start_time.strftime("%Y%m%d-%H%M%S"))
@@ -156,7 +155,7 @@ class geneticAlgorithm(object):
 
 
 if __name__ == '__main__':
-    simulation = geneticAlgorithm(2, 10)
+    simulation = geneticAlgorithm(3, 2)
 
     ####################
     # Single Mode.
@@ -169,17 +168,17 @@ if __name__ == '__main__':
     #### Many Mode.
     ####################
     paramList = [
-        [0.01, 0.25],
-        [0.02, 0.25],
-        [0.03, 0.25],
-        [0.04, 0.25],
-        [0.05, 0.25],
-        [0.06, 0.25],
-        [0.07, 0.25],
-        [0.08, 0.25],
-        [0.09, 0.25],
-        [0.10, 0.25],
-        [0.11, 0.25]
+        [0.01, 0.25]
+        # [0.02, 0.25],
+        # [0.03, 0.25],
+        # [0.04, 0.25],
+        # [0.05, 0.25]
+        # [0.06, 0.25],
+        # [0.07, 0.25],
+        # [0.08, 0.25],
+        # [0.09, 0.25],
+        # [0.10, 0.25],
+        # [0.11, 0.25]
     ]
     simulation.run_simulations_with_params(paramList, variable="Mutability")
     
