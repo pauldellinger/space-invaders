c9a1dfdd4862edda16883fd4d4d8be8556e767c1
Davis Booth <davis.booth@duke.edu>
2020-11-10 23:09:35 -0500

    stat stuff

--- geneticAlgo.py
+++ geneticAlgo.py
@@ -1,6 +1,7 @@
 from strategy import Strategy
 from random import randint
 from math import ceil
+from stats import *
 import gym
 import numpy as np
 from datetime import datetime
@@ -21,7 +22,7 @@ class geneticAlgorithm(object):
         self.env = gym.make('SpaceInvaders-v0')
         self.env.reset()
         self.start_time = datetime.now()
-
+    
     def evaluateScore(self, strategy):
         observation = np.array(self.env.reset())
         score = 0
@@ -68,6 +69,63 @@ class geneticAlgorithm(object):
             # for strategy in self.strategies:
             #     strategy.mutate()
         return results
+    
+    # Run many sims.
+    def run_simulations_with_params(self, paramList, variable="Selectivity"):
+        # Init.
+        allSimStats = []
+
+        # Each sim.
+        for specs in paramList:
+            self.SELECTIVITY = specs[1]                     # Set Selectivity.
+            self.strategies = []                            # Clear.
+            for i in range(0, NUM_STRATEGIES):
+                s = Strategy(MUTATE_PROBABILITY=specs[0])   # Set Mutability.
+                self.strategies.append(s)
+            
+            # Run.
+            results = self.startSimulation()
+            
+            # Stats.
+            self.export_results(results)
+            stats = fetch_simulation_stats(results)
+            
+            # Append.
+            print(stats)
+            allSimStats.append(stats)
+        
+        # Build.
+        varLst = [i[variable=="Selectivity"] for i in paramList]
+        legendLabels = list(map(lambda x: variable + "=" + str(x), varLst))
+        for stat in ('avg', 'max'):
+            Ys = []
+            Xs = []
+            for i in range(len(allSimStats)):
+                Ys.append([])
+                for j in range(len(allSimStats[i])):
+                    Ys[i].append(allSimStats[i][j][stat])
+                Xs.append([range(len(allSimStats[i]))])
+
+            d = {
+                
+                "X": Xs,
+                "Y":Ys,
+                "title" : variable + "'s effect on " + stat + "score",
+                "xLabel" : "Generation",
+                "yLabel" : stat + "Score",
+                "n" : len(allSimStats),
+                "legendLabels" : legendLabels
+            }
+            plt = make_plot(d['title'], d['xLabel'], d['yLabel'])
+            compiled_results = zip(d['Xs'], d['Ys'], d['legendLabels'])
+                #compiled_results = (d['Xs'][i], d['Ys'][i], label)
+            add_lines(plt, compiled_results)
+            save_plot(plt, stat + variable + self.start_time.strftime("%Y%m%d-%H%M%S"))
+            
+        
+        # Ret.
+        return allSimStats
+    
     def export_results(self, results):
         now = self.start_time.strftime("%Y%m%d-%H%M%S")
         for gen, generation in enumerate(results):
@@ -86,8 +144,33 @@ class geneticAlgorithm(object):
 
 if __name__ == '__main__':
     simulation = geneticAlgorithm(2, 10)
-    results = simulation.startSimulation()
-    simulation.export_results(results)
+
+    ####################
+    # Single Mode.
+    ####################
+    # results = simulation.startSimulation()
+    # simulation.export_results(results)
+    
+
+    ####################
+    #### Many Mode.
+    ####################
+    paramList = [
+        [0.01, 0.25],
+        [0.02, 0.25],
+        [0.03, 0.25],
+        [0.04, 0.25],
+        [0.05, 0.25],
+        [0.06, 0.25],
+        [0.07, 0.25],
+        [0.08, 0.25],
+        [0.09, 0.25],
+        [0.10, 0.25],
+        [0.11, 0.25]
+    ]
+    simulation.run_simulations_with_params(paramList, variable="Mutability")
+    
+
     
 
 
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from pylab import figure, axes, pie, title, show
 import numpy as np
+import statistics
 
 def save_plot(chart, path):
     chart.savefig('plots/' + path + '.png', bbox_inches='tight')
@@ -9,7 +10,52 @@ def save_plot(chart, path):
 def save_array(arr, path):
     np.savetxt("results/" + path + '.csv', arr, delimiter=",")
 
-def generation_stats(strategy):
+
+# Gen Stats.
+def fetch_generation_stats(strategies):
+    # Extract.
+    scores = [t[0] for t in strategies]
+    
+    # Compute.
+    maxx = max(scores)
+    minn = min(scores)
+    avg = sum(scores) / float(len(scores))
+    std = statistics.stdev(scores)
+
+    # Construct.
+    d = {
+        "max" : maxx,
+        "min" : minn,
+        "avg" : avg,
+        "std" : std
+    }
+
+    # Ret.
+    return d
+
+
+
+# Sim Stats.
+def fetch_simulation_stats(generations):
+    # Init.
+    allStats = []
+    
+    # Each Gen.
+    for generation in generations:
+        # This Stats.
+        thisStats = fetch_generation_stats(generation)
+
+        # Append.
+        allStats.append(thisStats)
+    
+    # Ret.
+    return allStats
+
+
+
+
+
+
         
 if __name__ == '__main__':
     # figure(1, figsize=(6, 6))
